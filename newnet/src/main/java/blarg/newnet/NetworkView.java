6777b1d4fb538e551450273ef0db6be470a3dad3
cymrucoder <git.cmt@gmail.com>
2018-12-30 20:47:54 +0000

    Replace colour differences for weights to use fixed red/green with width of the line representing intensity.

--- newnet/src/main/java/blarg/newnet/NetworkView.java
+++ newnet/src/main/java/blarg/newnet/NetworkView.java
@@ -1,8 +1,10 @@
 package blarg.newnet;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,6 +32,7 @@ public class NetworkView extends JFrame {
     
     public NetworkView(Network network) {
         this.network = network;
+        network.adjustForError(0.0);
         DrawCanvas dc = new DrawCanvas();
         dc.setPreferredSize(new Dimension(NETWORK_VIEW_WIDTH, NETWORK_VIEW_HEIGHT));
         setContentPane(dc);
@@ -49,21 +52,37 @@ public class NetworkView extends JFrame {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             
+            Graphics2D g2 = (Graphics2D) g;// Needs to be Graphics2D so we can use setStroke() later on
+                        
             int layerOffset = (int) ((double) NETWORK_VIEW_WIDTH / (double) (network.layers.size() + 1));// This splits the panel up neatly and avoids the edges
             int neuronRadius = 20;
             
             for (int i = 0; i < network.layers.size(); i++) {
                 int neuronOffset = (int) ((double) NETWORK_VIEW_HEIGHT / (double) (network.layers.get(i).getNumberOfNeurons() + 1));
                 int previousLayerNeuronOffset = (i == 0 ? -1 : (int) ((double) NETWORK_VIEW_HEIGHT / (double) (network.layers.get(i - 1).getNumberOfNeurons() + 1)));// Can't check previous layer if we're on layer 0
-                g.setColor(Color.BLACK);
-                
+                                
                 for (int j = 0; j < network.layers.get(i).getNumberOfNeurons(); j++) {
+                    g.setColor(Color.BLACK);
                     g.fillOval((layerOffset * (i + 1)) - neuronRadius, (neuronOffset * (j + 1)) - neuronRadius, neuronRadius * 2, neuronRadius * 2);
                     
                     if (i == 0) {// Input layer will have proper lines at some point
                         
                     } else {
                         for (Map.Entry<Integer, Double> entry : network.layers.get(i).getNeuron(j).getWeights().entrySet()) {// Draw a line for each connection backwards from this node
+                            float weight = entry.getValue().floatValue();
+                            
+                            if (weight == 0.0) {// Green is +ive, red if -ive, fade out grey if 0
+                                g.setColor(Color.LIGHT_GRAY);
+                                g2.setStroke(new BasicStroke(0.1f));
+                            } else if (weight > 0.0) {
+                                g.setColor(new Color(0.0f, 1.0f, 0.0f));
+                                g2.setStroke(new BasicStroke((weight * weight) * 5.0f));// Square the weight to exaggerate the differences, multiplier so you can actually see something
+                            } else {
+                                //float redVal = 0.7f + ((-weight / 10.0f) * 3.0f);
+                                g.setColor(new Color(1.0f, 0.0f, 0.0f));
+                                g2.setStroke(new BasicStroke((weight * weight) * 5.0f));
+                            }
+                            
                             g.drawLine((layerOffset * (i + 1)) - neuronRadius, neuronOffset * (j + 1), (layerOffset * i) + neuronRadius, (previousLayerNeuronOffset * (entry.getKey() + 1)));
                         }
                     }
